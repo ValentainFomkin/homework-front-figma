a3bad1f7642acc13d362ed99c05ec8e837254019
ValentainFomkin <valentain1625@mail.ru>
2025-01-22 19:14:23 +0300

    hw1

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -9,30 +9,34 @@ export type MessagePropsType = {
 
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
+    const {message} = props
+
+    const name = message.user.name
+    const text = message.message.text
+
     return (
-        <div id={'hw1-message-' + props.message.id} className={s.message}>
+        <div id={'hw1-message-' + message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <div className={s.image}>
-                    <img
-                        id={'hw1-avatar-' + props.message.id}
-                        src={props.message.user.avatar}
+                    <img id={'hw1-avatar-' + message.id}
+                         src={message.user.avatar}
                     />
                 </div>
                 <div className={s.text}>
-                    <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {props.message.user.name}
+                    <div id={'hw1-name-' + message.id} className={s.name}>
+                        {name.trim() !== '' && name}
 
                         {/**/}
                     </div>
-                    <div id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {props.message.message.text}
+                    <div id={'hw1-text-' + message.id} className={s.messageText}>
+                        {text.trim() !== '' && text}
 
                         {/**/}
                     </div>
                 </div>
             </div>
-            <div id={'hw1-time-' + props.message.id} className={s.time}>
-                {props.message.message.time}
+            <div id={'hw1-time-' + message.id} className={s.time}>
+                {message.message.time}
 
                 {/**/}
             </div>
